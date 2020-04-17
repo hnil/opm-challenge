4b63c2b4c722e0c4537d90631f3a58e22fd0962f
hnil <Halvor.M.Nilsen@sintef.no>
2020-04-17 08:14:56 +0200

    fixed error introduced in egg. added protypt for a simplified version of norne which should be possible to refine. Wells need fixing

--- scripts/egg/norneSimple.m
+++ scripts/egg/norneSimple.m
@@ -2,15 +2,21 @@ mrstModule add ad-blackoil ad-core mrst-gui ad-props deckformat blackoil-sequent
 mrstModule add linearsolvers agmg blackoil-sequential incomp
 mrstVerbose true
 %% egg
-
-
-[G, rock, fluid, deck] = setupEGG('realization', 0);
-G = computeBoundingBoxes(G);
-%rock.perm = mean(rock.perm).*ones(G.cells.num, 3);
+opm = mrstPath('opm-tests');
+assert(~isempty(opm), 'You must register https://github.com/opm/opm-tests as a module!');
+[deck, output] = getDeckOPMData('norne', 'NORNE_ATW2013');
+%%
+deck.RUNSPEC=rmfield(deck.RUNSPEC,{'EQLOPTS','ENDSCALE','SATOPTS','GRIDOPTS'})
+deck.GRID=rmfield(deck.GRID,{'FAULTS','MULTFLT','FLUXNUM','PINCH'})
+deck.PROPS=rmfield(deck.PROPS,{'SWATINIT','EHYSTR'});%%,'SCALECRS'});
+G = initEclipseGrid(deck,'SplitDisconnected',false);
+G = computeGeometry(G);    
+rock  = initEclipseRock(deck);
+rock  = compressRock(rock, G.cells.indexMap);
 [state0, model, schedule, nonlinear] = initEclipseProblemAD(deck, 'G', G, 'TimestepStrategy', 'none');
 
-W = schedule.control.W;
-W = addTrajectories(W, model.G, 2);
+
+
 
 
 %% refine
@@ -19,69 +25,74 @@ W = addTrajectories(W, model.G, 2);
 gridfromdeck=true;
 refine=[1,1,1];
 deck_new = rmfield(deck,'SCHEDULE');
-physdims=deck_new.GRID.cartDims.*[deck_new.GRID.DX(1),deck_new.GRID.DY(1),deck_new.GRID.DZ(1)]
-if(gridfromdeck)
-    grdecl = simpleGrdecl(deck_new.GRID.cartDims, 0, 'flat',true','undisturbed',true,'physdims',physdims);
-    grdecl.ZCORN=min(deck.GRID.TOPS)+grdecl.ZCORN;
-    deck_new.GRID=rmfield(deck_new.GRID,{'DX','DY','DZ','TOPS'})
-    deck_new.GRID.ZCORN=grdecl.ZCORN;
-    deck_new.GRID.COORD=grdecl.COORD;
-    deck_new = refineDeck(deck_new,refine)
-    %%
-    G_new = initEclipseGrid(deck_new);
-    G_new = computeGeometry(G_new);
-    rock_new  = initEclipseRock(deck_new);
-    rock_new  = compressRock(rock_new, G_new.cells.indexMap);
-    G_new=processGRDECL(deck_new.GRID);
-else
-    %% to do refine an mrst grid
-    error('refinement of mrst grid not implemented');
-    [G_new, rock_new] = deal(G, rock); % or something
-    G_new=cartGrid(refine.*G.cartDims,physdims);
-    G_new.nodes.coords(:,3)=G_new.nodes.coords(:,3)+min(deck.GRID.TOPS);
-end
+deck_new = refineDeck(deck_new,refine)
+G_new = initEclipseGrid(deck_new,'SplitDisconnected',false);
+G_new = computeGeometry(G_new);
+rock_new  = initEclipseRock(deck_new);
+rock_new  = compressRock(rock_new, G_new.cells.indexMap);
+%G_new=processGRDECL(deck_new.GRID);
+%%
 figure(2),clf,plotGrid(G_new)   
     %%
 G_new=computeGeometry(G_new);
 G_new = computeBoundingBoxes(G_new);
 
-W_new = [];
+schedule_new=schedule;
 maxperf=0;
-for k = 1:numel(W)
-    w = W(k);
-    tmp = computeTraversedCellsNew(G_new, w.trajectory);
-    W_new   = addWell(W_new, G_new, rock_new, tmp.cell, 'name', w.name, 'type', w.type, 'sign', ...
-                      w.sign, 'val', w.val, 'refDepth', w.refDepth, ...
-                      'compi', w.compi, 'lims', w.lims, ...
-                      'lineSegments', bsxfun(@times, tmp.vec, tmp.weight) );
-    maxperf=max(maxperf,numel(W_new(end).cells));              
+for i=1:numel(schedule.control)
+    W_new = [];
+    W = schedule.control(i).W;
+    W = addTrajectories(W, model.G, 2);
+    for k = 1:numel(W)
+        w = W(k);
+        newperfs=true;
+        if(i>1)
+            w_prev=W_prev(k);
+            if(all(w.cells == w_prev.cells) &&  all(w.WI == w_prev.WI))
+                newperfs=false;
+            end
+        end
+        if(newperfs)
+            tmp = computeTraversedCellsNew(G_new, w.trajectory);
+            W_new   = addWell(W_new, G_new, rock_new, tmp.cell, 'name', w.name, 'type', w.type, 'sign', ...
+                w.sign, 'val', w.val, 'refDepth', w.refDepth, ...
+                'compi', w.compi, 'lims', w.lims, ...
+                'lineSegments', bsxfun(@times, tmp.vec, tmp.weight) );
+            maxperf=max(maxperf,numel(W_new(end).cells));
+        else
+            wnew=W_prev_new(k);
+            wnew.type = W(k).type;
+            wnew.val = W(k).val;
+            wnew.cstatus=W(k).status;
+            wnew.cstatus=W(k).compi;
+            W_new=[W_new;wnew];
+        end
+    end
+    schedule_new.control(i).W=W_new;
+    W_prev=W;
+    W_prev_new=W_new;
 end
 disp('new wells calculated')
-%%
+%
 nstep=-5;
 mkdir('tmp')
-case_name='EGG'
+case_name='NORNE'
 outputprefix=fullfile(pwd(),'tmp',case_name);
 mkdir(outputprefix)
 model_new=model;
 model_new.G=G_new;
-schedule_new = schedule
-schedule_new.control.W=W_new;
-if(nstep>0)
-schedule_new.step.control=schedule_new.step.control(1:nstep)
-schedule_new.step.control=schedule_new.step.val(1:nstep)
+if(nstep>0)  
+    schedule_new.step.control=schedule_new.step.control(1:nstep)
+    schedule_new.step.val=schedule_new.step.val(1:nstep)
+    mcont=max(schedule_new.step.control);
+    schedule_new.control=schedule_new.control(1:mcont);
 end
+%%
 deck_new = model2Deck(model_new, schedule_new, 'deck', deck_new,'gridfromdeck',true)
 deck_new.RUNSPEC.WELLDIMS(2)=100;
 writeDeck(deck_new, outputprefix)
 %
-pth = getDatasetPath('egg');
-%deckfile_org=fullfile(pth,'MRST','Egg_Model_ECL.DATA');
-%deck_org=readEclipseDeck(deckfile_org);
-mkdir('tmp_sims')
 deckfile=fullfile(outputprefix,[case_name,'.DATA']);
-%deck_new_sim = readEclipseDeck(deckfile);
-%
 outputdir=fullfile(pwd(),'tmp_sims',case_name);
 simulator='/home/hnil/Documents/GITHUB/OPM/opm_source/master/builds/release_mpi/opm-simulators/bin/flow'
 [wellsols, states, reports_opm, extra] = runDeckOPM(deckfile,...
